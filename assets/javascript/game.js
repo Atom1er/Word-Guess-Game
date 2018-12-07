50ae4d011f1e3971ae6b4f0da51a38a63692e9c3
Thomas <at93om@gmail.com>
2018-12-06 23:56:25 -0500

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,131 +1,76 @@
-var indication =["Capital of landlocked country in West Africa called Burkina Faso", "Country of Colonel Gaddafi, it is a contry in North Africa, bordered by the Mediterranean Sea to the north,"]
-var question = ["what is the main City?", "what is the name of this contry?"]
-var word = ["OUAGADOUGOU", "LIBYAN"];
-var GuessWord1 = word[0].split("");
-var GuessWord2 = word[1].split("");
-var win = 0;
-var lost = 0;
-var userGuess = document.getElementById("answer");
-var Question = document.getElementById("Question");
-var indic = document.getElementById("hint");
-var score = document.getElementById("win");
-var lost = document.getElementById("lost");
-var life = document.getElementById("live");
-var f =0;
-var t =0;
-var l=10;
-var KeyName;
+  
+   //WordLibrary, random word to be Guess 
+        var indication = ["I am a landlocked country in West Africa, my capital is called OUAGADOUGOU", "Country of Colonel Gaddafi, it is a contry in North Africa, bordered by the Mediterranean Sea to the north", "From BigCats: I am the King of jungle", "zone of transition in Africa between the Sahara to the north and the Sudanian Savanna to the south"];
+        var question = ["what is the name of this Country?", "what is the name of this contry?", "What is my name?", "What is the name of this place?"];
+        var word = ["BURKINA", "LIBYAN", "LION", "SAHEL"];
+        var userkey = document.getElementById("answer");
+        var lost = document.getElementById("lost");
+        var indic = document.getElementById("hint");
+        var score = document.getElementById("win");
+        var life = document.getElementById("live");
+        var l = 10;
+        var m = 0;
+        var w = 0;
+        var correct = [];
+        var incorrect =[];
+        var ran = Math.floor(Math.random() * word.length);
+        var randomWord = word[ran];
+        let actualWord  = randomWord.split('');
+        
+    console.log(actualWord);
 
-
-var ran = Math.floor(Math.random() * word.length);
-// for (var p=0; p<l; p++){
-Result();
-if(ran == 0 && l>0){
-    Question.innerText=(question[0]);
-    indic.innerText=(indication[0]);
-    var Ans = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"]
-    userGuess.innerText = (Ans);
-    document.addEventListener('keypress', (event) => {
-        KeyName = event.key.toUpperCase();
-        // userGuess.innerText = (KeyName);
-        Result();
-        if(KeyName=='A'){
-            // alert('cool');
-            Ans[2]=["A"];
-            Ans[4]=["A"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        } else if(KeyName == 'D'){
-            Ans[5]=["D"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        }else if(KeyName == 'G'){
-            Ans[3]=["G"];
-            Ans[8]=["G"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        }else if(KeyName == 'O'){
-            Ans[0]=["O"];
-            Ans[6]=["O"];
-            Ans[9]=["O"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        } else if(KeyName == 'U'){
-            Ans[1]=["U"];
-            Ans[7]=["U"];
-            Ans[10]=["U"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        } else{
-            f++;
-            Result();
+    var Underscor = [];
+    var Underscore  = () => {
+        for (let i = 0; i < actualWord.length; i++) {
+               Underscor.push('_');         
         }
-    });
-    
-}
+        return Underscor;
+    }
+    console.log(Underscore());
+    userkey.innerHTML = Underscor;
+    lost.innerHTML = ("You have missed " +m);
+    life.innerHTML = (l + " remaining attempt");
 
-
-//2nd word
-
-else if(ran == 1 && l>0){
-    Question.innerText=(question[1]);
-    indic.innerText=(indication[1]);
-    var Ans = ["_", "_", "_", "_", "_", "_"]
-    userGuess.innerText = (Ans);
-    document.addEventListener('keypress', (event) => {
-        KeyName = event.key.toUpperCase();
-        // userGuess.innerText = (KeyName);
-        Result();
-        if(KeyName=='A'){
-            // alert('cool');
-            Ans[4]=["A"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        } else if(KeyName == 'B'){
-            Ans[2]=["B"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        }else if(KeyName == 'I'){
-            Ans[1]=["I"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        }else if(KeyName == 'L'){
-            Ans[0]=["L"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        } else if(KeyName == 'N'){
-            Ans[5]=["N"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        }else if(KeyName == 'Y'){
-            Ans[3]=["Y"];
-            userGuess.innerText = (Ans);
-            t++;
-            Result();
-        } else{
-            f++;
-            Result();
+    for(var q = 0; q < word.length; q++){
+        if(ran == q){
+        Question.innerText=(question[q]);
+        indic.innerText=(indication[q]);
         }
-    });
-}
 
-function Result(){
-    var l=10;
-    l=l-f;
-    life.innerText = ('You have '+ l +' tentatives remaining');
-    score.innerText = ('You found '+ t +' letters');
-    lost.innerText = ('You missed '+ f +' letters');
-}
+    }
+
+    document.addEventListener("keypress", (event) => {
+        let UserEnt = String.fromCharCode(event.keyCode);
+        let UserGuess = UserEnt.toUpperCase();
+        UserGuess.toUpperCase();
+        // console.log(UserGuess);
+        if(actualWord.indexOf(UserGuess) > -1){
+            correct.push(UserGuess);
+            Underscor[actualWord.indexOf(UserGuess)] = UserGuess;
+            userkey.innerHTML = Underscor;
+            // console.log(correct);
+            if(Underscor.join(',') == actualWord) {
+                alert("Congratulation You Got It!!!");
+                w++;
+            }
+            console.log(Underscor);
+        } else{
+            incorrect.push(UserGuess);
+            console.log(incorrect);
+            m++;
+            l-= 1;
+            life.innerHTML = (l + " remaining attempt");
+            lost.innerHTML = ("You have missed " +m);
+        }
+        if(l<1){
+            alert("Game Over");
+            var tryagain =prompt("Try again?");
+            if(tryagain){
+                location.reload(true);
+            }
+        }
+        
+    })
 
 
 
